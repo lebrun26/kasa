d2926e917a817a1a71b22284ae91a018804faca2
lebrun26 <lebrun.patrickdev@gmail.com>
2024-05-22 15:49:45 +0200

    mise en place de l'image sur la page d'accueil avec le filtre d'opacité et le texte

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
+import HomePicture from "../components/HomePicture";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <div>
+        <HomePicture />
+      </div>
       <Footer />
     </div>
   );
